170cfef97ae1047a4769db3e153b4f4936efb941
Shivam Kalra <shivamkalra98@gmail.com>
2021-03-12 16:16:37 +0530

    refactoring graphql code

--- index.js
+++ index.js
@@ -2,34 +2,10 @@ const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
-const Post = require("./models/Post");
-const User = require("./models/User");
 const { MONGODB } = require("./config");
+const resolvers = require("./graphql/resolvers");
 
-const typeDefs = gql`
-    type Post{
-        id:ID!
-        body:String!
-        createdAt: String!
-        username: String!
-
-    }
-    type Query {
-        getPosts: [Post]
-    }
-`;
-const resolvers = {
-    Query: {
-        getPosts: async() => {
-            try{
-                const posts = await Post.find();
-                return posts;
-            }catch(err){
-                throw new Error(err);
-            }
-        },
-    },
-};
+const typeDefs = require("./graphql/typeDefs");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
