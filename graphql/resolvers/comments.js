6b85f712677eff2ab4019d9a111d32321e551c23
Shivam Kalra <shivamkalra98@gmail.com>
2021-03-18 23:38:07 +0530

    add remove comment mutation

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const { UserInputError } = require("apollo-server-errors");
+const { UserInputError, AuthenticationError } = require("apollo-server-errors");
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
 module.exports = {
@@ -25,6 +25,29 @@ module.exports = {
                 await post.save();
                 return post;
             } else throw new UserInputError("Post not found");
+        },
+        deleteComment: async (parent, { postId, commentId }, context) => {
+            const { username } = checkAuth(context);
+            try {
+                const post = await Post.findById(postId);
+                if (post) {
+                    console.log({ post });
+                    const commentIndex = post.comments.findIndex(
+                        (comment) => comment.id === commentId
+                    );
+                    if (post.comments[commentIndex].username === username) {
+                        post.comments.splice(commentIndex, 1);
+                        await post.save();
+                        return post;
+                    } else {
+                        throw new AuthenticationError("Action not allowed");
+                    }
+                } else {
+                    throw new UserInputError("Post not found");
+                }
+            } catch (err) {
+                throw new Error(err);
+            }
         },
     },
 };
