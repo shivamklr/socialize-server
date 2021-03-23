d13b40c8ab48cded0793c67b5c10003be1087797
Shivam Kalra <shivamkalra98@gmail.com>
2021-03-23 16:36:03 +0530

    addition of like count and comment count

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -5,20 +5,21 @@ module.exports = gql`
         body: String!
         createdAt: String!
         username: String!
-        comments:[Comment]!
-        likes:[Like]!
-
+        comments: [Comment]!
+        likes: [Like]!
+        likeCount: Int!
+        commentCount: Int!
     }
-    type Comment{
-        id:ID!
-        createdAt:String!
-        username:String!
-        body:String!
+    type Comment {
+        id: ID!
+        createdAt: String!
+        username: String!
+        body: String!
     }
-    type Like{
-        id:ID!
-        createdAt:String!
-        username:String!
+    type Like {
+        id: ID!
+        createdAt: String!
+        username: String!
     }
     type User {
         id: ID!
@@ -36,15 +37,15 @@ module.exports = gql`
 
     type Query {
         getPosts: [Post]
-        getPost(postId: ID!):Post
+        getPost(postId: ID!): Post
     }
     type Mutation {
         register(registerInput: RegisterInput): User!
         login(username: String!, password: String!): User!
-        createPost(body:String!):Post!
-        deletePost(postId:ID!):String!
-        createComment(postId:ID!, body:String!):Post!
-        deleteComment(postId:ID!, commentId:ID!):Post!
-        likePost(postId:ID!):Post!
+        createPost(body: String!): Post!
+        deletePost(postId: ID!): String!
+        createComment(postId: ID!, body: String!): Post!
+        deleteComment(postId: ID!, commentId: ID!): Post!
+        likePost(postId: ID!): Post!
     }
 `;
